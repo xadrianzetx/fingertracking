d2b69a7f1463097d51ac753a7148a96075405fdd
Adrian Zuber <xadrianzetx@gmail.com>
2019-04-27 17:30:28 +0200

    model compiled for tf 1.11.0+

--- fingertracking/model.py
+++ fingertracking/model.py
@@ -1,37 +1,36 @@
 import cv2
 import pkg_resources
+import numpy as np
 import tensorflow as tf
+from tensorflow.python.saved_model import loader
 
 
 class HandTracker:
 
     def __init__(self, **kwargs):
-        self._model = kwargs.get('model', default='ssd_mobilenetv2')
+        self._model = kwargs.get('model', 'ssd_mobilenet')
         self._sess, self._graph = self._load_graph()
 
     def _load_graph(self):
-        # model src
-        # https://github.com/victordibia/handtracking/blob/master/model-checkpoint/ssdlitemobilenetv2/frozen_inference_graph.pb
-        path = 'assets/{}.pb'.format(self._model)
+        # src
+        # https://github.com/victordibia/handtracking/blob/master/model-checkpoint/ssdlitemobilenetv2
+        path = 'assets/{}/'.format(self._model)
         abs_path = pkg_resources.resource_filename('fingertracking', path)
-        graph = tf.Graph()
 
-        with graph.as_default():
-            gf = tf.gfile.GFile(abs_path, 'rb')
-            g = gf.read()
-            graph_def = tf.GraphDef()
-            graph_def.ParseFromString(g)
-            tf.import_graph_def(graph_def)
-            gf.close()
+        with tf.Graph().as_default() as graph:
+            sess = tf.Session(graph=graph)
+            loader.load(sess, [tf.saved_model.tag_constants.SERVING], abs_path)
 
-        return tf.Session(graph=graph_def), graph
+        return sess, graph
 
     def track(self, frame):
         rgb_frame = cv2.cvtColor(frame, cv2.COLOR_BGR2RGB)
+        expanded = np.expand_dims(rgb_frame, axis=0)
+
         image_tensor = self._graph.get_tensor_by_name('image_tensor:0')
         detection_boxes = self._graph.get_tensor_by_name('detection_boxes:0')
 
         # TODO reshape before run
-        bbox = self._sess.run([detection_boxes], feed_dict={image_tensor: rgb_frame})
+        bbox = self._sess.run(detection_boxes, feed_dict={image_tensor: expanded})
 
         return bbox
